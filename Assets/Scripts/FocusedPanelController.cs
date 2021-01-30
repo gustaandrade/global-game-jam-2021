8c824ee7a5c4c79d2a3e896d28b1e92fac0e5de2
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2021-01-30 16:00:06 -0300

    feat: adds base logic for changing panels

---
+++ Assets/Scripts/FocusedPanelController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FocusedPanelController : MonoBehaviour
+{
+  [Header("Public References")]
+  public List<FocusedPanelType> PanelStates;
+
+  private void Awake()
+  {
+    PanelStates = new List<FocusedPanelType>();
+  }
+
+  private void ChangeToState(FocusedPanel nextPanel)
+  {
+    PanelStates.ForEach(ps => ps.gameObject.SetActive(false));
+
+    if (nextPanel != FocusedPanel.Off)
+      PanelStates.FirstOrDefault(ps => ps.PanelType == nextPanel).gameObject.SetActive(true);
+  }
+}
+
+[Serializable]
+public enum FocusedPanel
+{
+  Computer,
+  Printer,
+  Person,
+  Off
+}

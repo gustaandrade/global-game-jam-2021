9135851cc1000335650dfa29f11d997a40c5d906
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2021-01-31 03:06:27 -0300

    feat: update constants file with prompts

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,6 +2,96 @@
 
 public static class Constants
 {
+  public static List<string> RequestBaseTexts = new List<string> {
+      "My crush has",
+  };
+
+  public static List<string> RequestHairTypeTexts = new List<string> {
+    "a curly",
+    "a long",
+    "a short",
+    "a medium"
+  };
+
+  public static List<string> RequestHairColorTexts = new List<string> {
+    "brown hairstyle",
+    "black hairstyle",
+    "blonde hairstyle",
+    "gray hairstyle",
+    "red hairstyle",
+  };
+
+  public static List<string> RequestBeardTypeTexts = new List<string> {
+    "a thin",
+    "a pirate",
+    "a thick",
+    "a fluff"
+  };
+
+  public static List<string> RequestBeardColorTexts = new List<string> {
+    "brown moustache",
+    "black moustache",
+    "blonde moustache",
+    "gray moustache",
+    "red moustache",
+  };
+
+  public static List<string> RequestEyesTypeTexts = new List<string> {
+    "squinted",
+    "lively",
+    "drowsy",
+    "wild"
+  };
+
+  public static List<string> RequestEyesColorTexts = new List<string> {
+    "brown eyes",
+    "black eyes",
+    "blonde blue",
+    "green eyes",
+    "gray eyes",
+  };
+
+  public static List<string> RequestEyebrowTypeTexts = new List<string> {
+    "a thin eyebrow",
+    "a thick eyebrow",
+    "a bushy eyebrow",
+    "a unibrow"
+  };
+
+  public static List<string> RequestHeadTypeTexts = new List<string> {
+    "a circular face",
+    "a round face",
+    "a square face"
+  };
+
+  public static List<string> RequestNoseTypeTexts = new List<string> {
+    "a ractangle nose",
+    "a triangle nose",
+    "a circular nose",
+    "a flat nose"
+  };
+
+  public static List<string> RequestMouthTypeTexts = new List<string> {
+    "a dry smile",
+    "an innocent smile",
+    "voluptuous lips",
+    "a timid smile",
+    "a big smile"
+  };
+
+  public static List<string> RequestDetailTypeTexts = new List<string> {
+    "wrinkles",
+    "a beauty mark"
+  };
+
+  public static List<string> RequestSkinToneTypeTexts = new List<string> {
+    "is asian",
+    "is black",
+    "is latinx",
+    "is white",
+    "is an alien"
+  };
+
   public static List<string> RequestIntroBaseTexts = new List<string> {
       "Hi there, can you help me?",
       "Is this the Crush Booth™?",
